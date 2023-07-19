6092a1a2e20347a1dc90b3017b00978bf9c65657
Romanded <romanabo099@gmail.com>
2023-07-19 13:42:43 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <EditingForm/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import EditingForm from "@/components/EditingForm.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      EditingForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  div {
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+
 </style>
---
+++ src/components/EditingForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <form>
+        <h1>{{list[i].value}}</h1>
+        <label for="select">Выберите элемент</label>
+        <select name="select"  @change="e => selectValue(e.target.value)">
+            <option :value="i-1" :key="list[i-1]" v-for="i in list.length" >{{ list[i-1].name }}</option>
+        </select>
+        <label for="input">Измените его значение</label>
+        <input name="input" type="text" v-model="list[i].value"/>
+    </form>
+</template>
+
+<script>
+import { data } from "@/utils/data";
+
+export default {
+    data: () => ({
+        list: data,
+        element: data[0],
+        i: 0,
+    }),
+    methods: {
+        selectValue(j) {
+            console.log(j)
+            this.i = j
+        }
+    },
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+
+        border-radius: 10px;
+        background: #ffffff;
+        box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.25);
+    }
+    h1 {
+        margin: 0 auto 20px;
+    }
+    select {
+        width: 367px;
+        border: none;
+        outline: none;
+        border-bottom: 1px solid grey;
+        margin-bottom: 20px;
+    }
+    input {
+        border: none;
+        outline: none;
+        border-bottom: 1px solid grey;
+    }
+    label {
+        margin-bottom: 5px;
+    }
+</style>
